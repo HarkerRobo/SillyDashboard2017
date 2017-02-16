e7b66bb4e9d69567f22ec9ddf236616ee575c04e
Ryan Adolf <ryanadolf123@gmail.com>
2017-02-16 13:54:21 -0800

    Resize windows if they begin as too small

--- src/window_monitor.py
+++ src/window_monitor.py
@@ -7,9 +7,14 @@ STREAMS = (DRIVER, VISION) = range(2)
 
 TOLERANCE = 0
 
-WIDTHS = {
-    DRIVER: 970, #1296, # The laptop's screen resolution is limitted, so the window will be limited to 970 px across.
+EXPECTED_WIDTHS = {
+    DRIVER: 970, # The laptop's screen resolution is limitted, so the window will be limited to 970 px across.
     VISION: 640
+}
+
+DESIRED_SIZES = {
+    DRIVER: (1296, 972),
+    VISION: (640, 480)
 }
 
 POSITIONS = {
@@ -30,20 +35,26 @@ def callback(hWinEventHook, event, hwnd, idObject, idChild, dwEventThread, dwmsE
     handled.append(hwnd) #Append the window id
     width, height = h.windowsize(hwnd)
 
-    clientw, _ = h.clientsize(hwnd)
+    clientw, clienth = h.clientsize(hwnd)
 
     # Find which stream the window is for
     stream = None
-    for s in WIDTHS:
-        if abs(WIDTHS[s] - clientw) <= TOLERANCE:
+    for s in EXPECTED_WIDTHS:
+        if abs(EXPECTED_WIDTHS[s] - clientw) <= TOLERANCE:
             stream = s
 
     if stream is None:
         print('Window with width {} not handled'.format(clientw))
         return
 
-    # Reposition the window
-    x, y = POSITIONS[stream](width)
-    h.user32.MoveWindow(hwnd, x, y, width, height, True)
+    # Reposition and possibly resize the window
+    vpad = height - clienth
+    hpad = width - clientw
+
+    w = DESIRED_SIZES[stream][0] + hpad
+    h = DESIRED_SIZES[stream][1] + vpad
+
+    x, y = POSITIONS[stream](w)
+    h.user32.MoveWindow(hwnd, x, y, w, h, True)
 
 h.register_hook(callback)
