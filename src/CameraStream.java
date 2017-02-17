6d1981c85ffa4cdd9c49c948c139b9b0fd6a1ce4
Ryan Adolf <ryanadolf123@gmail.com>
2017-02-17 14:01:15 -0800

    Update shared submodule

--- src/CameraStream.java
+++ src/CameraStream.java
@@ -19,57 +19,57 @@ import org.freedesktop.gstreamer.State;
 public class CameraStream extends JPanel {
 	private JLayeredPane layeredPane = null;
 	private JLabel statusLabel;
-	
+
 	private JPanel controlPanel;
-	
+
 	private Bin pipe;
-	
+
 	public CameraStream(String name, final RaspiNetworker networker, int streamPort, int width, int height, boolean showCorners) {
 		createStream(streamPort, width, height);
-		
+
 		networker.addStatusReceiver(new RaspiNetworker.StatusReceiver() {
-			
+
 			@Override
 			public void receiveStatus(String status) {
 				statusLabel.setText("<html>" + status + "</html>");
 			}
 		});
-		
+
 		setLayout(new BorderLayout());
-		
+
 		if (showCorners) {
 			layeredPane = new JLayeredPane();
-			
+
 			CornerViewer cv = new CornerViewer(networker, width);
 			layeredPane.add(cv, new Integer(1));
-			
+
 			add(layeredPane, BorderLayout.CENTER);
 		}
 
 		// ISO
 		final JSpinner isoField = new JSpinner(new SpinnerNumberModel(RaspiNetworker.ISO, 100, 800, 1));
 		JLabel isoLabel = new JLabel("ISO");
-		
+
 		JPanel isoPanel = new JPanel();
 		isoPanel.setLayout(new BorderLayout());
 		isoPanel.add(isoLabel, BorderLayout.WEST);
 		isoPanel.add(isoField, BorderLayout.CENTER);
-		
+
 		// Shutter speed
 		final JSpinner shutterField = new JSpinner(new SpinnerNumberModel(RaspiNetworker.SHUTTER, 100, 6000000, 100));
 		JLabel shutterLabel = new JLabel("Shutter speed");
-		
+
 		JPanel shutterPanel = new JPanel();
 		shutterPanel.setLayout(new BorderLayout());
 		shutterPanel.add(shutterLabel, BorderLayout.WEST);
 		shutterPanel.add(shutterField, BorderLayout.CENTER);
-		
+
 		// ISO + Shutter speed panel
 		JPanel shutterIsoPanel = new JPanel();
 		shutterIsoPanel.setLayout(new BorderLayout());
 		shutterIsoPanel.add(isoPanel, BorderLayout.EAST);
 		shutterIsoPanel.add(shutterPanel, BorderLayout.WEST);
-		
+
 		// Start button
 		JButton startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener() {
@@ -78,7 +78,7 @@ public class CameraStream extends JPanel {
 				networker.reconnect((Integer) isoField.getValue(), (Integer) shutterField.getValue());
 			}
 		});
-		
+
 		// Stop button
 		JButton stopButton = new JButton("Stop");
 		stopButton.addActionListener(new ActionListener() {
@@ -87,13 +87,13 @@ public class CameraStream extends JPanel {
 				networker.disconnect();
 			}
 		});
-		
+
 		// Button panel
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(1, 2));
 		buttonPanel.add(startButton);
 		buttonPanel.add(stopButton);
-		
+
 		// ISO + Shutter speed + button panel
 		controlPanel = new JPanel();
 		controlPanel.setLayout(new BorderLayout());
@@ -105,21 +105,21 @@ public class CameraStream extends JPanel {
 		// Status
 		statusLabel = new JLabel();
 		statusLabel.setPreferredSize(new Dimension(0, 48));
-		
+
 		setBorder(BorderFactory.createTitledBorder(name));
-		
+
 		setLayout(new BorderLayout());
 		add(controlPanel, BorderLayout.NORTH);
 		add(statusLabel, BorderLayout.CENTER);
-		
+
 		networker.reconnect(RaspiNetworker.ISO, RaspiNetworker.SHUTTER);
 	}
-	
+
 	private void createStream(int port, int width, int height) {
         pipe = Bin.launch("udpsrc port=" + port + " ! application/x-rtp, payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink", false);
         pipe.play();
 	}
-	
+
 	public void close() {
 		pipe.setState(State.NULL);
 	}
